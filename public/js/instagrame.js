7548a922d98b28e80f380f98cf8205cd735f6fa1
Marc-Alban <millet.marcalban@gmail.com>
2019-12-09 09:47:48 +0100

    Modification instagram api pour un ajout de 10 maximum

--- public/js/instagrame.js
+++ public/js/instagrame.js
@@ -15,42 +15,42 @@ let postComments = document.getElementById("postComments");
 let postHtml = '';
 
 function nFormatter(num) {
-    if (num >= 1000000000) {
-        return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
-    }
-    if (num >= 1000000) {
-        return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
-    }
-    if (num >= 1000) {
-        return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
-    }
-    return num;
+  if (num >= 1000000000) {
+    return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
+  }
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+  }
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+  }
+  return num;
 }
 
-xhr.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-        let user = this.response.graphql.user;
-        image.src = user.profile_pic_url;
-        username.innerHTML = user.username;
-        name.innerHTML = user.full_name;
-        biography.innerHTML = user.biography;
-        nbPost.innerHTML = user.edge_owner_to_timeline_media.count;
-        followers.innerHTML = user.edge_followed_by.count;
-        following.innerHTML = user.edge_follow.count;
-        // Affichage des posts avec une boucle car c'est un tableau
-        post = user.edge_owner_to_timeline_media.edges;
-        for (let i = 0; i < post.length; i++) {
-            let url = post[i].node.display_url;
-            let likes = post[i].node.edge_liked_by.count;
-            let comments = post[i].node.edge_media_to_comment.count;
-            postImg.src = url;
-            postLikes.innerHTML = nFormatter(likes);
-            postComments.innerHTML = nFormatter(comments);
-        }
-
-    } else if (this.readyState === 4 && this.status === 404) {
-        alert('Erreur: 404: /');
+xhr.onreadystatechange = function() {
+  if (this.readyState === 4 && this.status === 200) {
+    let user = this.response.graphql.user;
+    image.src = user.profile_pic_url;
+    username.innerHTML = user.username;
+    name.innerHTML = user.full_name;
+    biography.innerHTML = user.biography;
+    nbPost.innerHTML = user.edge_owner_to_timeline_media.count;
+    followers.innerHTML = user.edge_followed_by.count;
+    following.innerHTML = user.edge_follow.count;
+    // Affichage des posts avec une boucle car c'est un tableau
+    post = user.edge_owner_to_timeline_media.edges;
+    for (let i = 0; i < 10; i++) {
+      let url = post[i].node.display_url;
+      let likes = post[i].node.edge_liked_by.count;
+      let comments = post[i].node.edge_media_to_comment.count;
+      postImg.src = url;
+      postLikes.innerHTML = nFormatter(likes);
+      postComments.innerHTML = nFormatter(comments);
     }
+
+  } else if (this.readyState === 4 && this.status === 404) {
+    alert('Erreur: 404: /');
+  }
 }
 
 xhr.open("GET", "https://www.instagram.com/3bbigbangbourse?__a=1", true);
