0567c413d8f5356146621c67b1fc3927ed208200
Marc-Alban <millet.marcalban@gmail.com>
2019-12-12 16:50:05 +0100

    Modification du routeur

--- src/tools/Router.php
+++ src/tools/Router.php
@@ -6,6 +6,7 @@ class Router
 {
     private $url;
     private $page;
+    private $id;
     private $pageFront;
     private $pageBack;
     private $routes = [];
@@ -13,16 +14,19 @@ class Router
     public function __construct(string $url)
     {
         $this->url = $url;
+        $this->id = $_GET['id'] ?? null;
         $this->page = $_GET['page'] ?? 'home';
         $this->pageFront = ['home', 'article', 'blog', 'page', 'contact', 'newPassword', 'lostPassword', '404'];
         $this->pageBack = ['dashboard', 'table', 'partenaire', 'login', 'graph', 'form'];
     }
 
-    public function get(string $path = null, string $action = null, ?array $data = null)
+    public function get(string $path = null, string $action = null, ?array $datas = null)
     {
         if (in_array($this->page, $this->pageFront) || empty($_GET['page'])) {
-            $this->routes[$path] = ['action' => $action, 'data' => $data];
-        } else if (in_array($this->page, $this->pageBack)) {
+            $this->routes[$path] = ['action' => $action, 'data' => $datas];
+        }
+
+        if (isset($datas) && !empty($datas) && $datas !== null) {
 
         }
     }
@@ -35,21 +39,23 @@ class Router
 
     public function match()
     {
-
         foreach ($this->routes as $key => $val) {
             if ($key === $this->url) {
-                $elements = explode('@', $val);
-                $this->callController($elements);
+                $element = explode('@', $val['action']);
+                $datas = $val['data'];
+                $this->callController($element, $datas);
             }
         }
     }
 
-    private function callController(array $elements)
+    private function callController(array $elements, ?array $datas = null)
     {
+        var_dump($elements, $datas);
+        die();
         $className = 'App\Controller\\' . $elements[0];
         $method = $elements[1];
         $controller = new $className;
-        $controller->$method($this->get('', '', []));
+        $controller->$method($this->get('', '', [$datas]));
         // if ($_SERVER["REQUEST_METHOD"] === "GET") {
         // } else if ($_SERVER["REQUEST_METHOD"] === "POST") {
         //     $controller->$method($this->post('', '', []));
