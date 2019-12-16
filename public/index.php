1fc88229fc9c1c902f99dd7f808c0ae51f8501c9
Marc-Alban <millet.marcalban@gmail.com>
2019-12-16 16:27:57 +0100

    Test d'une regex dans le fichier htaccess

--- public/index.php
+++ public/index.php
@@ -2,8 +2,8 @@
 require_once '../vendor/autoload.php';
 use App\Tools\Router;
 $Router = new Router($_SERVER["REQUEST_URI"]);
-// var_dump($_GET);
-// die();
+var_dump($_GET);
+die();
 $Router->get('/', "Index@home");
 $Router->get('/home', "Index@home");
 $Router->get('/home/{id}', "Index@home");
