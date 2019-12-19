70445f488ee091021b65baf202b2c22f5681aef5
Marc-Alban <millet.marcalban@gmail.com>
2019-12-19 13:43:11 +0100

    Passage du routeur comme pour le p4, mais bug

--- public/index.php
+++ public/index.php
@@ -2,21 +2,24 @@
 declare (strict_types = 1);
 require_once '../vendor/autoload.php';
 use App\Tools\Router;
+
 $Router = new Router();
+var_dump($Router, $_GET);
+die();
+$Router->controller();
 
 //GET
 //On met un slash pour eviter de se tromper si l'url est vide ..
-$Router->url('/', 'Home@homeAction');
-$Router->url('/home', 'Home@homeAction'); // pas possible avec {$id} --> le faire en js
-$Router->url('/listesArticles', 'Article@listesArticlesAction'); //articles
-$Router->url('/listesArticles/article/{id}', 'Article@articleAction'); //blog/article/1
-$Router->url('/page/{slug}', 'Page@pageRenderAction'); //Slug au lieu de id
-$Router->url('/lostPassword', 'Password@lostPassAction');
-$Router->url('/newPassword', 'Password@newPassAction');
-$Router->url('/contact', 'Contact@contactRenderAction');
+// $Router->url('/', 'Home@homeAction');
+// $Router->url('/home', 'Home@homeAction'); // pas possible avec {$id} --> le faire en js
+// $Router->url('/listesArticles', 'Article@listesArticlesAction'); //articles
+// $Router->url('/listesArticles/{article}/{id}', 'Article@articleAction'); //blog/article/1
+// $Router->url('/page/{slug}', 'Page@pageRenderAction'); //Slug au lieu de id
+// $Router->url('/lostPassword', 'Password@lostPassAction');
+// $Router->url('/newPassword', 'Password@newPassAction');
+// $Router->url('/contact', 'Contact@contactRenderAction');
 //POST->
 // $Router->post('/lostPassword/{action}', 'Password@lostPassAction');
 // $Router->post('/newPassword/{action}', 'Password@newPassAction');
 // $Router->post('/contact/{action}', 'Contact@contactRenderAction');
-
-$Router->run();
+// $Router->run();
--- src/tools/Router.php
+++ src/tools/Router.php
@@ -1,98 +1,63 @@
 <?php
 declare (strict_types = 1);
-
 namespace App\Tools;
 
-use App\Controller\HomeController;
+session_start();
 
 class Router
 {
-    //Les routes dans un tableau vide
-    private $routes = [];
-    private $params;
+    private $id;
+    private $action;
+    private $page;
+    private $pageFront;
+    private $pageBack;
+    private $controllerName;
+    private $controller;
+    private $methode;
 
-    /**
-     * Récupère une route et l'insère dans le tableau des routes
-     * En index la route, et en valeur le controller
-     * @param string $route
-     * @param string $controller
-     * @return void
-     */
-    public function url(String $route, String $controller): void
+    public function __contruct()
     {
-        $route = trim($route, '/');
-        $this->routes[$route] = $controller;
+        $this->id = $_GET['id'] ?? null;
+        $this->action = $_GET['action'] ?? null;
+        $this->page = $_GET['page'] ?? 'home';
+        $this->pageFront = ['home', 'blog', 'error'];
+        $this->pageBack = ['adminComments', 'adminChapters', 'adminChapter', 'adminWrite', 'login', 'adminProfil'];
+        //var_dump($this->page, $this->pageFront);
+
     }
 
-    /**
-     * Renvoie le resultat de la méthode match
-     *
-     *
-     * @return void
-     */
-    public function run()
+    public function controller(): string
     {
-        foreach ($this->routes as $route => $controller) {
-            //Si vrai on execute une action qui est une dernière méthode
-            if ($this->match($_GET['url'], $route)) {
-                return $this->callController($controller);
+        var_dump($this->page, $this->pageFront);
+        if (in_array($this->page, $this->pageFront) || empty($this->page) || !in_array($this->page, $this->pageBack)) {
+            //var_dump('test');
+            //return $this->controllerName = 'App\Controller\FrontendController' . $this->page . "Controller"
+        } else if (in_array($this->page, $this->pageBack)) {
+            return $this->controllerName = 'App\Controller\BackendController';
+        }
+        //die();
+    }
+
+    public function call()
+    {
+        $this->controller = new $this->controllerName;
+        $this->methode = $this->page . 'Action';
+    }
+
+    public function Action()
+    {
+        if (in_array($this->page, $this->pageFront) || in_array($this->page, $this->pageBack)) {
+            if (($this->action === null && $this->id === null) || ($this->action !== null && $this->id !== null || $this->action !== null && $this->id === null)) {
+                $this->controller->$this->methode($_SESSION, ['get' => $_GET, 'post' => $_POST, 'files' => $_FILES]);
+            } else if ($this->action === null && $this->id !== null) {
+                $this->controller->$this->methode($_SESSION, ['post' => $_POST, 'get' => $_GET]);
             }
         }
-        return $this->error();
     }
 
-    /**
-     * Permet de comparer l'url à la propriété $route
-     * Renvoie false si c'est pas bon sinon renvoi true
-     * et insere les paramètres dans un tableau
-     *
-     * @param string $url
-     * @param string $route
-     * @return void
-     */
-    private function match(String $url, String $route): bool
+    public function error()
     {
-        //on doit récupérer si il y a un paramètre après la route {[a-z]+}
-        //puis le remplacer par une regex
-        $path = preg_replace("#({[a-z]+})#", "([a-z0-9\-]+)\/?", $route);
-        //On verifie si le chemin correspons à l'url
-        if (!preg_match("#^$path$#", $url, $matches)) {
-            return false;
-        };
-        //On enlève la première ligne du tableau
-        array_shift($matches);
-        //Si paramètres vide --> = tableau vide
-        $this->params = $matches;
-        return true;
-    }
-
-/**
- * Permet d'appeler un controller
- *
- * @param string $controller
- * @return object
- */
-    private function callController(String $controller): ?object
-    {
-        $params = explode("@", $controller);
-        $controllerName = $params[0] . 'Controller';
-        $controllerString = 'App\Controller\\' . $controllerName;
-        $controller = new $controllerString();
-        //Appel d'une méthode en fournissant les paramètres si il y en a
-        return call_user_func_array([$controller, $params[1]], $this->params);
-    }
-
-    /**
-     * La fonction redirectionErreur404() renvoit une véritable erreur 404
-     * passée en paramètre.
-     *
-     * @param : void
-     * @return : void
-     */
-    private function error()
-    {
-        $error = new HomeController();
-        return $error->errorAction();
+        $this->controller->errorAction();
     }
 
 }
