59b3ef90ce352af4edf7130a378273b824c4bc8c
Marc-Alban <millet.marcalban@gmail.com>
2020-02-03 12:57:20 +0100

    rectification bug encodage avec insertion sur fichier database le setname utf-8

--- Database.php
+++ Database.php
@@ -8,11 +8,14 @@ use \PDO;
 
 class Database
 {
-    const HOST = 'localhost';
-    const DATABASE = 'bmfinance';
-    const USER = 'root';
-    const PASSWORD = '';
+    const DSN = 'mysql:host=db5000285322.hosting-data.io;dbname=dbs278576;charset=utf8mb4';
+    const USER = 'dbu79211';
+    const PASSWORD = '479c329d49fa8297ab78ed988EF.67512';
     private static $instance = null;
+    // const DSN = 'mysql:host=localhost;dbname=bmfinance;charset=utf8mb4';
+    // const USER = 'root';
+    // const PASSWORD = '';
+    // private static $instance = null;
 
 /************************************PDO Connexion************************************************* */
     /**
@@ -25,8 +28,7 @@ class Database
     {
         try {
             if (self::$instance === null) {
-                $pdoOptions[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES utf8";
-                self::$instance = new PDO('mysql:host=' . self::HOST . ';dbname=' . self::DATABASE, self::USER, self::PASSWORD, $pdoOptions);
+                self::$instance = new PDO(self::DSN, self::USER, self::PASSWORD);
             }
             return self::$instance;
         } catch (Exception $e) {
